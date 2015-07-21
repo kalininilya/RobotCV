d2088a34e453e3e5c5e376261f9698993bf737c7
Илья Калинин <kalinination@gmail.com>
2015-07-21 11:34:57 +0300

    comments

--- main.cpp
+++ main.cpp
@@ -82,21 +82,17 @@ Point intersection(Point p1, Point p2, Point p3, Point p4) {
   double y1 = p1.y, y2 = p2.y, y3 = p3.y, y4 = p4.y;
 
   double d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-  // If d is zero, there is no intersection
   if (d == 0) return NULL;
 
-  // Get the x and y
   double pre = (x1*y2 - y1*x2), post = (x3*y4 - y3*x4);
   double x = (pre * (x3 - x4) - (x1 - x2) * post) / d;
   double y = (pre * (y3 - y4) - (y1 - y2) * post) / d;
 
-  // Check if the x and y coordinates are within both lines
   if (x < min(x1, x2) || x > max(x1, x2) ||
     x < min(x3, x4) || x > max(x3, x4)) return NULL;
   if (y < min(y1, y2) || y > max(y1, y2) ||
     y < min(y3, y4) || y > max(y3, y4)) return NULL;
 
-  // Return the point of intersection
   Point ret;
   ret.x = x;
   ret.y = y;
